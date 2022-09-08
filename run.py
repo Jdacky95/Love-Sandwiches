e094dead442be77ba3772408008c23a5ea8a782f
Jdacky95 <jamesdack88@gmail.com>
2022-09-08 12:10:23 +0000

    collect sales data for last 5 entries in sales columns

--- run.py
+++ run.py
@@ -81,7 +81,7 @@ def update_worksheet(data, worksheet):
     print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated succesfully." )
+    print(f"{worksheet} worksheet updated succesfully.")
   
 
 def calculate_surplus_data(sales_row):
@@ -103,6 +103,20 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries():
+
+    """
+    Collects columns of data from sales worksheet, 
+    collecting the last 5 entries for each sandwich and returns the data as a list of lists"
+    """
+
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
 
 
 def main():
